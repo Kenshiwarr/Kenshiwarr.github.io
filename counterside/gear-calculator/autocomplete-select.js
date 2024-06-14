6f6b84bc86e78b0108bbd947190ff24573ed7fbd
Kenshiwarr <168667839+Kenshiwarr@users.noreply.github.com>
2024-06-14 16:31:02 +0300

    Update autocomplete-select.js

--- counterside/gear-calculator/autocomplete-select.js
+++ counterside/gear-calculator/autocomplete-select.js
@@ -2014,7 +2014,7 @@ for (let i = 0; i < unit_totalAttacks.length; i++) {
     sDmg_chpd = (Math.round(Number(unitCalculatedDmg[j][9])))
     sDmg_mhpd = (Math.round(Number(unitCalculatedDmg[j][10])))
 
-    if ((i > 0) && (Number(cdSkill_list[i]) !== 0) && (Number(unit_totalAttacks[i][6]) < 100)) {
+    if ((i > 0) && (Number(cdSkill_list[i]) !== 0) && (Number(unit_totalAttacks[i][6].split(';')[0]) < 100)) {
       skill_cd_improve = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16"> <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0"/> </svg>'
     } else {
       $('.sci_skill_upt').off('.sci')
